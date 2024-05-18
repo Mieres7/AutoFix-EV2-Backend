ea5d6d1cdeef72bbb13ceec0bd5c8a8f7ef49ca7
vicente Mieres <vicente.mieres@usach.cl>
2024-05-17 23:36:49 -0400

    fix

--- ms-vehicle/src/main/java/com/vmieres/msvehicle/MsVehicleApplication.java
+++ ms-vehicle/src/main/java/com/vmieres/msvehicle/MsVehicleApplication.java
@@ -2,8 +2,10 @@ package com.vmieres.msvehicle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MsVehicleApplication {
 
 	public static void main(String[] args) {
