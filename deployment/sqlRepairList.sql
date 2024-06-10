e9ac344f0fa6185bd4adbac315ed83b46cf32cc8
Vicente <vicente.mieres@usach.cl>
2024-06-09 23:29:04 -0400

    fix

--- deployment/sqlRepairList.sql
+++ deployment/sqlRepairList.sql
@@ -29,17 +29,3 @@ INSERT INTO repair_type_cost (repair_type_cost_id, repair_type, description, gas
 (9, 'Air Conditioning and Heating System Repair', 'Includes refrigerant recharge, repairing or replacing the compressor, and troubleshooting heating system issues.', 150000, 150000, 180000, 180000),
 (10, 'Fuel System Repairs', 'Cleaning or replacing fuel injectors, repairing or replacing the fuel pump, and troubleshooting fuel supply issues.', 130000, 140000, 220000, 0),
 (11, 'Windshield and Glass Replacement', 'Repairing small cracks in the windshield or complete replacement of damaged windshields and windows.', 80000, 80000, 80000, 80000);
-
-INSERT INTO report (repair_type_name, month, year, sedan, sedan_amount, hatchback, hatchback_amount, pickup, pickup_amount, suv, suv_amount, van, van_amount) VALUES
-('Brake System Repairs', '12', '2023', 4, 480000, 5, 240000, 6, 360000, 3, 120000, 2, 240000),
-('Brake System Repairs', '01', '2024', 7, 360000, 8, 120000, 5, 240000, 6, 360000, 4, 480000),
-('Brake System Repairs', '02', '2024', 6, 240000, 7, 360000, 3, 480000, 5, 120000, 8, 240000),
-('Brake System Repairs', '03', '2024', 9, 360000, 3, 120000, 7, 240000, 4, 480000, 6, 360000),
-('Brake System Repairs', '04', '2024', 8, 120000, 5, 480000, 2, 360000, 9, 240000, 7, 360000),
-('Cooling System Service', '12', '2023', 3, 420000, 4, 300000, 5, 360000, 6, 240000, 7, 180000),
-('Cooling System Service', '01', '2024', 6, 360000, 7, 150000, 4, 300000, 5, 420000, 3, 480000),
-('Cooling System Service', '02', '2024', 5, 300000, 6, 420000, 7, 480000, 4, 150000, 2, 240000),
-('Cooling System Service', '03', '2024', 8, 360000, 2, 150000, 6, 300000, 5, 480000, 3, 420000),
-('Cooling System Service', '04', '2024', 7, 150000, 5, 480000, 3, 360000, 6, 240000, 4, 420000);
-
-
